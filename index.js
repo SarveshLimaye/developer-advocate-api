b9a1a3782dc5271beec048fd620b66d331739f4d
SarveshLimaye <sarveshlimaye2002@gmail.com>
2022-10-13 20:50:44 +0530

    Added middlewares for api :sparkles:

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var cors = require('cors');
 const express = require('express');
 const app = express();
+const errorHandlerMiddleware = require('./middleware/error-handler')
+const notFoundMiddleware = require('./middleware/not-found')
 
 app.use(express.json());
 app.use(cors())
@@ -11,5 +13,8 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
     }
 )
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
 app.listen(port , console.log(`Server started at port ${port}`))
