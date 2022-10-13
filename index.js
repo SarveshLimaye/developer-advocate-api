c1b4c6b74c7a277d95a0a9a40f452f4cb1363f4a
SarveshLimaye <sarveshlimaye2002@gmail.com>
2022-10-13 20:58:49 +0530

    Mongodb setup :hammer:

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+require('dotenv').config();
 var cors = require('cors');
 const express = require('express');
 const app = express();
+
+//middleware
 const errorHandlerMiddleware = require('./middleware/error-handler')
 const notFoundMiddleware = require('./middleware/not-found')
 
+//db
+const connectDB = require('./db/connect')
+
 app.use(express.json());
 app.use(cors())
-
-const port = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -17,4 +21,15 @@ app.get('/', (req, res) => {
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
 
-app.listen(port , console.log(`Server started at port ${port}`))
+const port = process.env.PORT || 5000
+
+const start = async () => {
+    try{
+       await connectDB(process.env.MONGODB_URI)
+      app.listen(port , console.log(`Server started at port ${port}`))
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+start();
