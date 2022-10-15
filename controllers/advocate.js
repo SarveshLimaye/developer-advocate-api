ad8556449dadd28705d9af6e529ce747de2a734a
SarveshLimaye <sarveshlimaye2002@gmail.com>
2022-10-15 22:40:15 +0530

    auto add advocates in companies + dynamic company content :sparkles:

--- controllers/advocate.js
+++ controllers/advocate.js
@@ -1,8 +1,18 @@
 const Advocate = require('../models/advocate');
+const Company = require('../models/company');
 const asyncHandler = require('express-async-handler');
 
 const getAllAdvocates = asyncHandler(async (req, res) => {
+    let  idComp
+    let href
     const advocates = await Advocate.find();
+    const fetchCompany = await Company.find({name : 'Agora'});
+ fetchCompany.map(company =>{
+        idComp = String(company._id)
+        href = `http://localhost:5000/companies/${idComp}`
+        console.log(href)
+        console.log(company.logo)
+    });
     res.status(200).json({ advocates });
 });
 
@@ -16,8 +26,29 @@ const getAdvocateById = asyncHandler(async (req, res) => {
     }
 });
 
+
+
 const addAdvocate = asyncHandler(async (req, res) => {
- const { name, profile_pic, short_bio, long_bio, advocate_year_experience, company, links } = req.body;
+ 
+ const { name, profile_pic, short_bio, long_bio, advocate_year_experience, companyName, links } = req.body;
+ let idComp
+ let href
+ let logo
+ const fetchCompany = await Company.find({name : companyName });
+fetchCompany.map(company =>{
+     idComp = String(company._id)
+     href = `http://localhost:5000/companies/${idComp}`
+     logo = company.logo
+ });
+
+ const company = {
+        id: idComp,
+        name: companyName,
+        logo: logo,
+        href: href
+ }
+ 
+ 
  const advocate = await Advocate.create({
         name,
         profile_pic,
@@ -28,6 +59,7 @@ const addAdvocate = asyncHandler(async (req, res) => {
         links
     });
     if (advocate) {
+        const updateCompany = await Company.findByIdAndUpdate(idComp, { $push: { advocates: advocate._id } }, { new: true });
        res.status(200).json({
             _id: advocate._id,
             name: advocate.name,
