d2a89d0be8b5a182e0f8b62ede60357663e38572
SarveshLimaye <sarveshlimaye2002@gmail.com>
2022-11-04 22:07:29 +0530

    updated index.js  :rocket:

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config({path: __dirname+'/.env'});
+  }
+  
 var cors = require('cors');
 const express = require('express');
 const app = express();
