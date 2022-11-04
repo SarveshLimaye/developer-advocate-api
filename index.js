f2de036e561c6484a9b02ba414373ab4d980a3ec
SarveshLimaye <sarveshlimaye2002@gmail.com>
2022-11-04 22:58:43 +0530

    build setting changed :rocket:

--- index.js
+++ index.js
@@ -40,9 +40,9 @@ const start = async () => {
 }
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend', 'build')));
+    app.use(express.static(path.join(__dirname, 'build')));
     app.get('/*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../frontend', 'build', 'index.html'));
+      res.sendFile(path.join(__dirname, 'build'));
     })
   }
 
